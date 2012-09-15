c980806af36645a7dec3b4de3553797ae89988d8
Dmitry Kurinskiy <name.alari@gmail.com>
2012-09-15 23:02:08 +0400

    angular-ui resources

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -3,4 +3,11 @@ modules = {
         resource url:'coffee/CreativeAtom.coffee'
         dependsOn "angular"
     }
+
+    'angular-ui'{
+        resource url:'js/angular-ui/angular-ui.min.js'
+        resource url:'js/angular-ui/angular-ui.min.css'
+        resource url:'js/angular-ui/angular-ui-ieshiv.min.js'
+        dependsOn "angular"
+    }
 }
